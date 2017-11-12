7751ba52d2054c264f11fb08adb1bd94ae8fdee6
GF808080 <noone@nomail.com>
2017-11-11 20:31:58 -0500

    added mapping function

--- scatter_chart2.py
+++ scatter_chart2.py
@@ -7,10 +7,12 @@ Created on Sun Nov  5 16:07:52 2017
 """
 
 import pandas as pd
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure, show, ColumnDataSource
+from bokeh.models import HoverTool
 from sqlalchemy import create_engine
 import math
 import json
+
 ## Pull in private data
 with open('config.json', 'r') as infile:
     config = json.loads(infile.read())
@@ -40,10 +42,21 @@ def make_houseplot(data, time):
         pdata = data[(data.hour >= 6) & (data.hour < 9)]
     elif str(time).lower() == 'evening':
         pdata = data[(data.hour >= 16) & (data.hour < 19)]
+    ##Tooltips
+    source = ColumnDataSource(data=dict(
+    x=list(data['start']),
+    y=list(data['drivetime'],
+    desc=['A', 'b', 'C', 'd', 'E'],
+    ))
+    
+    hover = HoverTool(tooltips=[
+        ("person", "$person"),
+        ("date", "$realtime"),
+        ("drivetime", "$drivetime"),
+    ])
     ##establish plot
-
     p = figure(title="Drive Times by House for {time}".format(time=time),\
-               x_range=houses, y_range=(0, 75), plot_width=600, plot_height=800)
+               x_range=houses, y_range=(0, 75), plot_width=800, plot_height=1200)
 
     #for personA
     data_pa= pdata[pdata.person==config['personA']]
